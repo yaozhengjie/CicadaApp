3f44b38bd9d551ffa21d4ab7215f9c3922fea880
llitfkitfk <llitfkitfk@gmail.com>
2014-12-10 15:57:45 +0800

    update add bicycle

--- app/src/main/java/com/cicada/bicycle/model/Bicycle.java
+++ app/src/main/java/com/cicada/bicycle/model/Bicycle.java
@@ -22,154 +22,139 @@ public class Bicycle {
     private long createTime;
     private long lastAccess;
 
-    public long getId()
-    {
+    public long getId() {
         return id;
     }
-    public void setId(long id)
-    {
+
+    public void setId(long id) {
         this.id = id;
     }
 
-    public String getTitle()
-    {
+    public String getTitle() {
         return title == null ? "" : title;
     }
-    public void setTitle(String title)
-    {
+
+    public void setTitle(String title) {
         this.title = title;
     }
 
-    public String getCatalog()
-    {
+    public String getCatalog() {
         return catalog == null ? "" : catalog;
     }
-    public void setCatalog(String catalog)
-    {
+
+    public void setCatalog(String catalog) {
         this.catalog = catalog;
     }
 
-    public String getBrand()
-    {
+    public String getBrand() {
         return brand == null ? "" : brand;
     }
-    public void setBrand(String brand)
-    {
+
+    public void setBrand(String brand) {
         this.brand = brand;
     }
 
-    public String getDeposit()
-    {
+    public String getDeposit() {
         return deposit == null ? "" : deposit;
     }
-    public void setDeposit(String deposit)
-    {
+
+    public void setDeposit(String deposit) {
         this.deposit = deposit;
     }
 
-    public String getRentPrice()
-    {
+    public String getRentPrice() {
         return rentPrice == null ? "" : rentPrice;
     }
-    public void setRentPrice(String rentPrice)
-    {
+
+    public void setRentPrice(String rentPrice) {
         this.rentPrice = rentPrice;
     }
 
-    public String getOriginPrice()
-    {
+    public String getOriginPrice() {
         return originPrice == null ? "" : originPrice;
     }
-    public void setOriginPrice(String originPrice)
-    {
+
+    public void setOriginPrice(String originPrice) {
         this.originPrice = originPrice;
     }
 
-    public String getAddress()
-    {
+    public String getAddress() {
         return address == null ? "" : address;
     }
-    public void setAddress(String address)
-    {
+
+    public void setAddress(String address) {
         this.address = address;
     }
 
-    public int getQuality()
-    {
+    public int getQuality() {
         return quality;
     }
-    public void setQuality(int quality)
-    {
+
+    public void setQuality(int quality) {
         this.quality = quality;
     }
 
-    public String getPhoto1()
-    {
+    public String getPhoto1() {
         return photo1;
     }
-    public void setPhoto1(String photo1)
-    {
+
+    public void setPhoto1(String photo1) {
         this.photo1 = photo1;
     }
-    public String getPhoto2()
-    {
+
+    public String getPhoto2() {
         return photo2;
     }
-    public void setPhoto2(String photo2)
-    {
+
+    public void setPhoto2(String photo2) {
         this.photo2 = photo2;
     }
-    public String getPhoto3()
-    {
+
+    public String getPhoto3() {
         return photo3;
     }
-    public void setPhoto3(String photo3)
-    {
+
+    public void setPhoto3(String photo3) {
         this.photo3 = photo3;
     }
 
-    public String getBicycleStatus()
-    {
+    public String getBicycleStatus() {
         return bicycleStatus;
     }
-    public void setBicycleStatus(String bicycleStatus)
-    {
+
+    public void setBicycleStatus(String bicycleStatus) {
         this.bicycleStatus = bicycleStatus;
     }
 
-    public String getDescription()
-    {
+    public String getDescription() {
         return description == null ? "" : description;
     }
-    public void setDescription(String description)
-    {
+
+    public void setDescription(String description) {
         this.description = description;
     }
 
-    public long getUserId()
-    {
+    public long getUserId() {
         return userId;
     }
-    public void setUserId(long userId)
-    {
+
+    public void setUserId(long userId) {
         this.userId = userId;
     }
 
-    public long getCreateTime()
-    {
+    public long getCreateTime() {
         return createTime;
     }
-    public void setCreateTime(long createTime)
-    {
+
+    public void setCreateTime(long createTime) {
         this.createTime = createTime;
     }
 
-    public long getLastAccess()
-    {
+    public long getLastAccess() {
         return lastAccess;
     }
-    public void setLastAccess(long lastAccess)
-    {
+
+    public void setLastAccess(long lastAccess) {
         this.lastAccess = lastAccess;
     }
 }
